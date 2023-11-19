2719131e0891e5802a2bf5b32b0d8531187ec303
Carrena <carrena1642@gmail.com>
2023-11-19 23:03:44 +0900

    f

---
+++ backend/src/main/java/carrena/orghart/OrghartApplication.java
@@ -0,0 +1,13 @@
+package carrena.orghart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrghartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrghartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/carrena/orghart/OrghartApplicationTests.java
@@ -0,0 +1,13 @@
+package carrena.orghart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrghartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
